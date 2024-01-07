043056849407614c3d7218ae06bad4a1b5d7f9aa
DATWY <121526575+DATWY@users.noreply.github.com>
2024-01-07 16:34:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
 from flask import Flask, render_template
-from telegram import Bot
-import time
-i = 0
+
 app = Flask(__name__)
-while i<100:
-    i+=1
-    new()
-    time.sleep(1)
+
 @app.route('/')
-def home():
-    # Replace 'TARGET_CHAT_ID' with the chat ID you want to send the greeting message to
-    target_chat_id = '-4066405074'
-    
-    # Send a greeting message
-    greeting_message = 'Hello, welcome to my Flask app!'
-    bot.send_message(chat_id=target_chat_id, text=greeting_message)
-    
-    return i
-def new():
-    return yes + i
+def index():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
