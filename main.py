b205ca6c8fb0836bea3e8222bdc0613e1e23fbd3
DATWY <121526575+DATWY@users.noreply.github.com>
2024-01-07 15:22:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from telegram import Bot
 
 app = Flask(__name__)
 
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token
+BOT_TOKEN = '6856621284:AAEBfMFjcy0iX8gMTVA-y935dhW548_3BaI'
+bot = Bot(token=BOT_TOKEN)
+
 @app.route('/')
-def index():
-    return "Choo Choo: Welcome to your Flask app test 🚅"
+def home():
+    # Replace 'TARGET_CHAT_ID' with the chat ID you want to send the greeting message to
+    target_chat_id = '-4066405074'
+    
+    # Send a greeting message
+    greeting_message = 'Hello, welcome to my Flask app!'
+    bot.send_message(chat_id=target_chat_id, text=greeting_message)
+    
+    return render_template('index.html', status='Message sent successfully')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True)
