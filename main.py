69bc393c9e488f6684612cb0908a42bbb1523a38
DATWY <121526575+DATWY@users.noreply.github.com>
2024-01-07 16:43:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
+
+messages = []
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/save-message', methods=['POST'])
+def save_message():
+    data = request.get_json()
+    messages.append(data)
+    return jsonify(success=True)
+
+@app.route('/get-messages')
+def get_messages():
+    return jsonify(success=True, messages=messages)
 
 if __name__ == '__main__':
     app.run(debug=True)
